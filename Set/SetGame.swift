c1309d7885e811a2b7a8e22539988944af61fbb0
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-12 22:11:24 +0300

    Implement a game of solo (i.e. one player) Set

---
+++ Set/SetGame.swift
@@ -0,0 +1,67 @@
+//
+//  SetGame.swift
+//  Set
+//
+//  Created by Dmitry Reshetnik on 12.04.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+//  A set consists of three cards satisfying all of these conditions:
+//  - They all have the same number or have three different numbers.
+//  - They all have the same shape or have three different shapes.
+//  - They all have the same shading or have three different shadings.
+//  - They all have the same color or have three different colors.
+
+import Foundation
+
+struct SetGame {
+    var playingSetCardDeck: [SetCard] = []
+    var selectedCards: [SetCard] = []
+    var matchedCards: [SetCard] = []
+    
+    /// Checking if selected cards are match according to game of Set rules.
+    mutating func checkSetCards(from selected: [SetCard]) {
+        if selected.count == 3 {
+//            if (((selected[0].number == selected[1].number) && (selected[1].number == selected[2].number)) || ((selected[0].number != selected[1].number) && (selected[0].number != selected[2].number) && (selected[1].number != selected[2].number)) && ((selected[0].shape == selected[1].shape) && (selected[1].shape == selected[2].shape)) || ((selected[0].shape != selected[1].shape) && (selected[0].shape != selected[2].shape) && (selected[1].shape != selected[2].shape)) && ((selected[0].shading == selected[1].shading) && (selected[1].shading == selected[2].shading)) || ((selected[0].shading != selected[1].shading) && (selected[0].shading != selected[2].shading) && (selected[1].shading != selected[2].shading)) && ((selected[0].color == selected[1].color) && (selected[1].color == selected[2].color)) || ((selected[0].color != selected[1].color) && (selected[0].color != selected[2].color) && (selected[1].color != selected[2].color))) {
+            if (((selected[0].number == selected[1].number) && (selected[1].number == selected[2].number)) || ((selected[0].number != selected[1].number) && (selected[0].number != selected[2].number) && (selected[1].number != selected[2].number))) {
+                for index in selected.indices {
+                    matchedCards.append(selected[index])
+                }
+            }
+        }
+    }
+    
+    /// Replace the selected cards if they are match or add 3 cards to the game.
+    mutating func dealSetCard() -> [SetCard] {
+        var deal: [SetCard] = []
+        
+        if selectedCards.count == 3 {
+            for index in selectedCards.indices {
+                if matchedCards.contains(selectedCards[index]), playingSetCardDeck.count > 0 {
+                    selectedCards[index] = playingSetCardDeck.remove(at: index)
+                } else if playingSetCardDeck.count > 0 {
+                    deal.append(playingSetCardDeck.remove(at: index))
+                }
+            }
+        } else if playingSetCardDeck.count > 0 {
+            for index in 0...2 {
+                deal.append(playingSetCardDeck.remove(at: index))
+            }
+        }
+        
+        return deal
+    }
+    
+    /// Create deck of 81 SetCard's.
+    init() {
+        for number in SetCard.Number.all {
+            for shape in SetCard.Shape.all {
+                for shade in SetCard.Shading.all {
+                    for color in SetCard.Color.all {
+                        playingSetCardDeck.append(SetCard(number: number.rawValue, shape: String(repeating: shape.rawValue, count: number.rawValue), shading: shade, color: color))
+                    }
+                }
+            }
+        }
+        playingSetCardDeck.shuffle()
+    }
+}
--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -13,8 +13,116 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for index in cardButtons.indices {
+            cardButtons[index].layer.cornerRadius = 8.0
+            cardButtons[index].layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+            cardButtons[index].setTitle(game.playingSetCardDeck[index].shape, for: UIControl.State.normal)
+            if index >= 12 {
+                cardButtons[index].setTitle("", for: UIControl.State.normal)
+                cardButtons[index].backgroundColor = #colorLiteral(red: 0.9952403903, green: 0.7589642406, blue: 0.1794864237, alpha: 0)
+                cardButtons[index].isEnabled = false
+            } else {
+                cardsOnScreen.append(game.playingSetCardDeck.remove(at: index))
+            }
+        }
     }
 
-
+    @IBOutlet weak var iPhoneLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var game: SetGame = SetGame()
+    var cardsOnScreen: [SetCard] = []
+    var selectedIndices: [Int] = []
+    var isMatched: Bool = false
+    
+    @IBAction func startNewGame(_ sender: UIButton) {
+        cardsOnScreen.removeAll()
+        selectedIndices.removeAll()
+        game = SetGame()
+        self.viewDidLoad()
+    }
+    
+    /// Allow to select cards to try to match as a Set, also support deselection.
+    @IBAction func selectCard(_ sender: UIButton) {
+        if let cardIndex = cardButtons.firstIndex(of: sender) {
+            let card: SetCard = cardsOnScreen[cardIndex]
+            
+            if game.selectedCards.count == 3 {
+                if isMatched {
+                    dealMoreCards(sender)
+                } else {
+                    for index in selectedIndices {
+                        cardButtons[index].layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+                    }
+                }
+                game.selectedCards.removeAll()
+                selectedIndices.removeAll()
+                game.selectedCards.append(card)
+                selectedIndices.append(cardIndex)
+                cardButtons[cardIndex].layer.borderWidth = 2.0
+                cardButtons[cardIndex].layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            } else {
+                if game.selectedCards.contains(card) {
+                    cardButtons[cardIndex].layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+                    game.selectedCards.removeAll { (selectedCard: SetCard) -> Bool in
+                        return selectedCard == card
+                    }
+                    selectedIndices.removeAll { (selectedIndex: Int) -> Bool in
+                        return selectedIndex == cardIndex
+                    }
+                } else {
+                    game.selectedCards.append(card)
+                    selectedIndices.append(cardIndex)
+                    if selectedIndices.count == 3 {
+                        checkAndIndicateCards()
+                    } else {
+                        cardButtons[cardIndex].layer.borderWidth = 2.0
+                        cardButtons[cardIndex].layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+                    }
+                }
+            }
+        }
+    }
+    
+    /// Replace the selected cards if they are match or add 3 cards to the game.
+    @IBAction func dealMoreCards(_ sender: UIButton) {
+        var cards: [SetCard] = game.dealSetCard()
+        
+        if cards.isEmpty {
+            for index in game.selectedCards.indices {
+                cardButtons[selectedIndices[index]].setTitle(game.selectedCards[index].shape, for: UIControl.State.normal)
+                cardButtons[selectedIndices[index]].layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+            }
+            game.selectedCards.removeAll()
+            selectedIndices.removeAll()
+        }
+        
+        for index in cardButtons.indices {
+            if cardButtons[index].currentTitle == "" && !cards.isEmpty {
+                cardButtons[index].setTitle(cards.first!.shape, for: UIControl.State.normal)
+                cardButtons[index].layer.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                cardButtons[index].isEnabled = true
+                cardsOnScreen.append(cards.removeFirst())
+            }
+        }
+    }
+    
+    /// Uses coloration to indicate whether cards are match or mismatch.
+    func checkAndIndicateCards() {
+        game.checkSetCards(from: game.selectedCards)
+        
+        for index in game.selectedCards.indices {
+            if game.matchedCards.contains(game.selectedCards[index]) {
+                cardButtons[selectedIndices[index]].layer.borderWidth = 3.0
+                cardButtons[selectedIndices[index]].layer.borderColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+                isMatched = true
+            } else {
+                cardButtons[selectedIndices[index]].layer.borderWidth = 3.0
+                cardButtons[selectedIndices[index]].layer.borderColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+                isMatched = false
+            }
+        }
+    }
 }
 
